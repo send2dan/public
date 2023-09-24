30a8bf21791e149111d7562bd172ad6f2f763081
Daniel Weiand <91281221+send2dan@users.noreply.github.com>
2023-09-24 23:05:06 +0100

    Producing Parameterised Reports using {purrr} and {quarto}
    Reprex of a workflow for producing parameterised reports using the {nycflights13} dataset (second attempt to get map2 to work)

--- parameterised_quarto_report_execute.R
+++ parameterised_quarto_report_execute.R
@@ -1,6 +1,8 @@
 
 # for one input, create runparams function with one input: origin ------------------------
 
+# create function to run a report against every subcategory of one variable, e.g. origin airport
+
 runparams_one_input <- function(origin) {
   quarto::quarto_render(
     "parameterised_quarto_report.qmd",
@@ -14,15 +16,15 @@ runparams_one_input <- function(origin) {
 
 runparams_one_input(origin = "JFK")
 
-# to run a report against every subcategory of a variable, e.g. origin airport
+# execute with purrr:map() function
 
 params_origin <- unique(flights$origin)
-
-# execute with purrr:map() function
 
 purrr::map(params_origin, runparams_one_input)
 
 # for two inputs, create runparams function with two inputs: origin and month -------------
+
+# create function to run a report against every subcategory of two variables, e.g. both origin airport and month
 
 runparams_two_inputs <- function(origin, month) {
   quarto::quarto_render(
@@ -33,14 +35,11 @@ runparams_two_inputs <- function(origin, month) {
   )
 }
 
-# to run a report against every subcategory of two variables, e.g. both origin airport and month
-
-params_origin <- unique(flights$origin)
-
-params_month <- unique(flights$month)
-
 # execute with purrr:map2() function
+# https://stackoverflow.com/questions/54256790/is-there-a-way-i-can-recycle-elements-of-the-shorter-list-in-purrr-map2-or-pur
 
-purrr::map2(params_origin, params_month, runparams_two_inputs)
-
+x <- unique(flights$origin)
+y <- unique(flights$month)
+l <- max(length(y), length(x))
+purrr::map2(rep(x,len = l), rep(y,len = l),runparams_two_inputs)
 
