07b8f195eca57d6dbfaa6d4e236c969ff6e3c4e5
Daniel Weiand <91281221+send2dan@users.noreply.github.com>
2023-11-07 17:13:32 +0000

    Add files via upload

--- misc/my_paths.R
+++ misc/my_paths.R
@@ -17,31 +17,14 @@ Sys.setenv(RENV_DOWNLOAD_METHOD = getOption("download.file.method"))
 #recheck downloader currently used by {renv}:
 renv:::renv_download_method()
 
-#activate {renv}
-if (!require("renv")) install.packages("renv", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
+# #activate {renv}
+if (!require("AMR")) install.packages(c("AMR"), repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
 
 # renv::init()
-renv::activate()
+# renv::activate()
+# renv::upgrade()
 
-# #load ggiraph package for interactive ggplot
-if (!require("nycflights13")) install.packages("nycflights13", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-# #load ggiraph package for interactive ggplot
-if (!require("tidymodels")) install.packages("tidymodels", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-# #load ggiraph package for interactive ggplot
-if (!require("ggiraph")) install.packages("ggiraph", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-# #install and load packages required by project using {pacman}
-if (!require("pacman")) install.packages("pacman", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-# #install and load devtools
-if (!require("devtools")) install.packages("devtools", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-# #install and load devtools
-if (!require("gghighlight")) install.packages("gghighlight", repos = "https://cran.ma.imperial.ac.uk/", type = "win.binary")
-
-pacman::p_load("flextable", "here", "readxl", "readr", "tidyverse", "tidymodels", "lubridate", "janitor", "AMR", "bibtex", "bookdown", "cleaner", "DiagrammeR", "fuzzyjoin", "ggsurvfit", "gtsummary", "kableExtra", "NHSRplotthedots", "pacman", "padr", "patchwork", "PHEindicatormethods", "qicharts2", "quarto", "markdown", "ragg", "RefManageR", "runcharter", "sciplot", "skimr", "svglite", "thematic", "tidycmprsk", "tidylog", "tidytext", "whisker", "xts", "zoo", "RSQLite", "lattice", "mgcv", "miniUI", "devtools", "Rdiagnosislist", "ggiraph", "gghighlight", "scales", "ggtext", "nycflights13")
+pacman::p_load("pacman", "tidyverse", "lubridate", "here", "knitr", "markdown", "quarto", "flextable", "readxl", "readr", "janitor", "skimr", "fontawesome", "beepr")
 
 ##optional {renv} snapshot
 # renv::snapshot(repos = "https://cran.ma.imperial.ac.uk/")
